e6079f6f1bdb2d7d9af398690cabd6f7a50a4ef4
rasheedsafwan <rasheedsafwan23@gmail.com>
2025-01-31 01:08:50 +0530

    Initial commit

---
+++ src/app/service/Expense.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class Expense(BaseModel):
+        """Information about a transaction made on any Card"""
+        amount: Optional[str] = Field(title="expense", description="Expense made on the transaction")
+        merchant: Optional[str] = Field(title="merchant", description="Marchant name whom the transaction has been made")
+        currency: Optional[str] = Field(title="currency", description="currency of the transaction")
+    
